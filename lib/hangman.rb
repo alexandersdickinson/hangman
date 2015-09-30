11b004f21e734a7786f1b3bd64128ab320e023d7
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-09-29 21:08:44 -0700

    Pass first spec.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,11 @@
 class Hangman
   @@words = %w(artichoke apple bolus donkey insomniac infarction domination dalliance traction terminus modus milieu illicit magazine catalogue chaos cavort carbuncle spelunker apiary mellifluous)
+  
+  def initialize()
+    @word = @@words[rand(@@words.length() - 1)]
+  end
+  
+  def word()
+    @word
+  end
 end
